cdcb13ab49952db0f89d71de11bff6c01891b115
equintanilla20 <equintanilla20@gmail.com>
2025-04-01 02:34:40 -0500

    Spring Initializr Build

---
+++ mtg-archive-server/src/main/java/org/eqdev/mtg_archive_server/MtgArchiveServerApplication.java
@@ -0,0 +1,13 @@
+package org.eqdev.mtg_archive_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgArchiveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgArchiveServerApplication.class, args);
+	}
+
+}
