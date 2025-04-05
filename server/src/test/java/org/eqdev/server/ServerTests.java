ca581b3f1b6406a58d46ed9e77c646c24595a9f7
Ezekiel Quintanilla <equintanilla20@gmail.com>
2025-04-04 23:53:50 -0500

    Debugging scalar type

--- server/src/test/java/org/eqdev/server/ServerTests.java
+++ server/src/test/java/org/eqdev/server/ServerTests.java
@@ -1,5 +1,6 @@
 package org.eqdev.server;
 
+import org.apache.commons.logging.Log;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,12 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-class ServerApplicationTests {
+class ServerTests {
+
+    Log log = org.apache.commons.logging.LogFactory.getLog(ServerTests.class);
 
 	@Test
 	void contextLoads() {
         // Log test successful
-        
+        log.info("Context load successful");
 	}
-
 }
